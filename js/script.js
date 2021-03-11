f2377056ea3cc39700cb5bf4fd18692b1c69e23e
Mirko <mirkosbaglia2001@gmail.com>
2021-03-11 20:13:08 +0100

    Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 var calcolaPrezzo = document.getElementById('calculate');
 
+// console.log(price)
+var finalPrice = 10;
+
+
 calcolaPrezzo.addEventListener('click', function(){
   // console.log('Ho cliccato qui');
 
+  // Stampa allert se non inserisci il nome
   var burgerName = document.getElementById('burger-name').value;
   console.log(burgerName);
 
@@ -10,23 +15,47 @@ calcolaPrezzo.addEventListener('click', function(){
     alert('Inserisci prima il nome del tuo burger ')
   }
 
+
+  //Checkbox  con i vari ingredienti
   var ingredients = document.getElementsByClassName('ingred');
   // console.log(ingredients);
-
   for (var i = 0; i < ingredients.length; i++) {
     // console.log(ingredients[i]);
     var ingredient = ingredients[i];
     // console.log(ingredient);
 
-    var isCheckedIngredien = ingredient.checked;
+    var isCheckedIngredient = ingredient.checked;
     // console.log(isCheckedIngredient);
 
-    var price = parseInt(isCheckedIngredien.dataset.price);
-    console.log(price);
-
+    var price = parseInt(ingredient.dataset.price); // come se fosse una cartella
+    // console.log(price);
+    if (isCheckedIngredient) {
+      finalPrice += price;          //prezzo + sovrapprezzo
+    }
   }
 
 
+  //coupons
+  var coupon = document.getElementById('burger-coupon').value;
+  console.log(coupon);
+  var arr =['123ABC', '456DEF', '789GHI'];
+  if (arr.includes(coupon)){   // includes determina se un array include un certo elemento
+    finalPrice = parseFloat(finalPrice /100 * 80).toFixed(2);
+
+  }
+
+  //Utilizzando il for
+  // for (var i = 0; i < arr.length; i++) {
+  //   var singleCoupon = arr[i];
+  //    // console.log(arr[i]);
+  //   if(coupon == singleCoupon){
+  //     finalPrice = parseFloat(finalPrice /100 * 80).toFixed(2);
+  //   }
+  // }
 
 
+
+  console.log(finalPrice);
+  var stampPrice = document.getElementById('price');
+  stampPrice.innerHTML= finalPrice;
 })
